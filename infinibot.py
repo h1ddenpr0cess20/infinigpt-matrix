00600614d416a7bbc3fcb2e1a78c0fbddb0e13e2
h1ddenpr0cess20 <h1ddenpr0cess2085@gmail.com>
2023-05-05 00:10:23 -0400

    code cleanup

--- infinibot.py
+++ infinibot.py
@@ -26,7 +26,87 @@ class MatrixGPT:
         
         # store chat history
         self.messages = {}
+
+        #prompt parts
         self.prompt = ("assume the personality of ", ".  roleplay and always stay in character unless instructed otherwise.  keep your first response short.")
+    
+    # get the display name for a user
+    async def display_name(self, user):
+        try:
+            name = await self.client.get_displayname(user)
+            return name.displayname
+        except Exception as e:
+            print(e)
+
+    # simplifies sending messages to the channel            
+    async def send_message(self, channel, message):
+         await self.client.room_send(
+            room_id=channel,
+            message_type="m.room.message",
+            content={"msgtype": "m.text", "body": message},
+        )
+
+    # run message through moderation endpoint
+    async def moderate(self, message):
+        flagged = False
+        if not flagged:
+            moderate = openai.Moderation.create(input=message) 
+            flagged = moderate["results"][0]["flagged"] #true or false
+        return flagged
+
+    # add messages to the history dictionary
+    async def add_history(self, role, channel, sender, message):
+        if channel in self.messages:
+            if sender in self.messages[channel]: #if this user exists in the history dictionary
+                self.messages[channel][sender].append({"role": role, "content": message}) #add the message
+            else:
+                self.messages[channel][sender] = [
+                    {"role": "system", "content": self.prompt[0] + self.personality + self.prompt[1]},
+                    {"role": role, "content": message}]
+        else:
+            self.messages[channel]= {}
+            self.messages[channel][sender] = {}
+            if role == "system":
+                self.messages[channel][sender] = [{"role": role, "content": message}]
+            else: #add personality to the new user entry
+                self.messages[channel][sender] = [
+                    {"role": "system", "content": self.prompt[0] + self.personality + self.prompt[1]},
+                    {"role": role, "content": message}]
+
+    # create GPT response
+    async def respond(self, channel, sender, message, sender2=None):
+        try:
+            #Generate response with gpt-3.5-turbo model
+            response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=message)    
+        except Exception as e:
+            print(e)
+        else:
+            #Extract response text and add it to history
+            response_text = response['choices'][0]['message']['content']
+            await self.add_history("assistant", channel, sender, response_text)
+            if sender2: #if the .x function was used
+                display_name = await self.display_name(sender2)
+            else: #normal .ai response
+                display_name = await self.display_name(sender)
+            response_text = display_name + ":\n" + response_text.strip()
+            #Send response to channel
+            try:
+                await self.send_message(channel, response_text)
+            except Exception as e: 
+                print(e)
+            #Shrink history list for token size management (also prevents rate limit error)
+            if len(self.messages[channel][sender]) > 14:
+                del self.messages[channel][sender][1:3]  #delete the first set of question and answers 
+
+    # change the personality of the bot
+    async def persona(self, channel, sender, persona):
+        try:
+            await self.messages[channel][sender].clear()
+        except:
+            pass
+        personality = self.prompt[0] + persona + self.prompt[1]
+        await self.add_history("system", channel, sender, personality) #add to the message history
+
     # tracks the messages in channels
     async def message_callback(self, room: MatrixRoom, event: RoomMessageText):
        
@@ -146,87 +226,11 @@ f'''{self.bot_id}, an OpenAI chatbot.
 Available at https://github.com/h1ddenpr0cess20/infinibot-matrix    
 ''')
 
-    # get the display name for a user
-    async def display_name(self, user):
-        try:
-            name = await self.client.get_displayname(user)
-            return name.displayname
-        except Exception as e:
-            print(e)
-
-    # simplifies sending messages to the channel            
-    async def send_message(self, channel, message):
-         await self.client.room_send(
-            room_id=channel,
-            message_type="m.room.message",
-            content={"msgtype": "m.text", "body": message},
-        )
-
-    # run message through moderation endpoint
-    async def moderate(self, message):
-        flagged = False
-        if not flagged:
-            moderate = openai.Moderation.create(input=message) 
-            flagged = moderate["results"][0]["flagged"] #true or false
-        return flagged
-
-    # add messages to the history dictionary
-    async def add_history(self, role, channel, sender, message):
-        if channel in self.messages:
-            if sender in self.messages[channel]: #if this user exists in the history dictionary
-                self.messages[channel][sender].append({"role": role, "content": message}) #add the message
-            else:
-                self.messages[channel][sender] = [
-                    {"role": "system", "content": self.prompt[0] + self.personality + self.prompt[1]},
-                    {"role": role, "content": message}]
-        else:
-            self.messages[channel]= {}
-            self.messages[channel][sender] = {}
-            if role == "system":
-                self.messages[channel][sender] = [{"role": role, "content": message}]
-            else: #add personality to the new user entry
-                self.messages[channel][sender] = [
-                    {"role": "system", "content": self.prompt[0] + self.personality + self.prompt[1]},
-                    {"role": role, "content": message}]
-
-    # create GPT response
-    async def respond(self, channel, sender, message, sender2=None):
-        try:
-            #Generate response with gpt-3.5-turbo model
-            response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=message)    
-        except Exception as e:
-            print(e)
-        else:
-            #Extract response text and add it to history
-            response_text = response['choices'][0]['message']['content']
-            await self.add_history("assistant", channel, sender, response_text)
-            if sender2: #if the .x function was used
-                display_name = await self.display_name(sender2)
-            else: #normal .ai response
-                display_name = await self.display_name(sender)
-            response_text = display_name + ":\n" + response_text.strip()
-            #Send response to channel
-            try:
-                await self.send_message(channel, response_text)
-            except Exception as e: 
-                print(e)
-            #Shrink history list for token size management (also prevents rate limit error)
-            if len(self.messages[channel][sender]) > 14:
-                del self.messages[channel][sender][1:3]  #delete the first set of question and answers 
-
-    # change the personality of the bot
-    async def persona(self, channel, sender, persona):
-        try:
-            await self.messages[channel][sender].clear()
-        except:
-            pass
-        personality = self.prompt[0] + persona + self.prompt[1]
-        await self.add_history("system", channel, sender, personality) #add to the message history
-
     # main loop
     async def main(self):
         # Login, print "Logged in as @alice:example.org device id: RANDOMDID"
         print(await self.client.login(self.password))
+
         # start listening for messages
         self.client.add_event_callback(self.message_callback, RoomMessageText)
         
@@ -252,7 +256,12 @@ if __name__ == "__main__":
     server = "https://matrix.org" #change if using different homeserver
     username = "@USERNAME:SERVER.TLD" 
     password = "PASSWORD"
-    channels = ["#channel1:SERVER.TLD", "#channel2:SERVER.TLD", "#channel3:SERVER.TLD", "!ExAmPleOfApRivAtErOoM:SERVER.TLD", ] #enter the channels you want it to join here
+
+    channels = ["#channel1:SERVER.TLD", 
+                "#channel2:SERVER.TLD", 
+                "#channel3:SERVER.TLD", 
+                "!ExAmPleOfApRivAtErOoM:SERVER.TLD", ] #enter the channels you want it to join here
+    
     personality = "an AI that goes above and beyond, named InfiniBot" #change to whatever suits your needs
     
     # create bot instance
--- launcher.py
+++ launcher.py
@@ -7,7 +7,12 @@ openai.api_key = "API_KEY"
 server = "https://matrix.org" #change if using different homeserver
 username = "@USERNAME:SERVER.TLD" 
 password = "PASSWORD"
-channels = ["#channel1:SERVER.TLD", "#channel2:SERVER.TLD", "#channel3:SERVER.TLD", "!ExAmPleOfApRivAtErOoM:SERVER.TLD", ] #enter the channels you want it to join here
+
+channels = ["#channel1:SERVER.TLD", 
+            "#channel2:SERVER.TLD", 
+            "#channel3:SERVER.TLD", 
+            "!ExAmPleOfApRivAtErOoM:SERVER.TLD", ] #enter the channels you want it to join here
+
 personality = "an AI that goes above and beyond, named InfiniBot" #change to whatever suits your needs
 
 # create bot instance
